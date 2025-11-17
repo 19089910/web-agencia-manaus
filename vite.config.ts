2bb1da8e88f3997199c04af61ad56339ea1023fa
Lucas Barbosa Ferreira <lucasbarbosaferreira.lb@gmail.com>
2025-11-17 16:03:01 -0400

    fix: vite.config bild

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
-  base: "/web-agencia-manaus/",
-
+export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-
   plugins: [react()],
-
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
